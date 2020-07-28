ed9c80c98a26b35c8b4f0b5efbd1c64ddb0e32aa
Emma <emmagray183@gmail.com>
2020-07-28 23:24:26 +0100

    Basic pages and header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Gruesome Comics" />
+    <p>Terrifically Terrifying</p>
+    <img src="https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="gruesome clown" height="200px" width="300px"/>
+  </div>
+  );
 }
